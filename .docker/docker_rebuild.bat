c38a2cceef5b9d1d15645c151b4e2896b7d371cc
lapotko <dlyaregistr123@bk.ru>
2023-08-10 23:11:41 +0700

    add docker folder

--- .docker/docker_rebuild.bat
+++ .docker/docker_rebuild.bat
@@ -1,6 +1,9 @@
 CALL mvn clean install -DskipTests
 
 docker rmi -f lapotkod/config-service:0.0.1
+
+cd ..
+
 cd .\config-service\
 docker build . -t lapotkod/config-service:0.0.1
 cd ..
@@ -9,5 +12,7 @@ docker rmi -f lapotkod/discovery-server:0.0.1
 cd .\discovery-server\
 docker build . -t lapotkod/discovery-server:0.0.1
 cd ..
+
+cd .\.docker\
 
 docker compose up
