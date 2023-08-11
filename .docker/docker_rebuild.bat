904863ffedcbce910836e70e15ca64f2a76efd57
lapotko <dlyaregistr123@bk.ru>
2023-08-11 22:34:17 +0700

    api gateway is up and running

--- .docker/docker_rebuild.bat
+++ .docker/docker_rebuild.bat
@@ -1,9 +1,8 @@
+cd ..
+
 CALL mvn clean install -DskipTests
 
 docker rmi -f lapotkod/config-service:0.0.1
-
-cd ..
-
 cd .\config-service\
 docker build . -t lapotkod/config-service:0.0.1
 cd ..
@@ -12,6 +11,12 @@ docker rmi -f lapotkod/discovery-server:0.0.1
 cd .\discovery-server\
 docker build . -t lapotkod/discovery-server:0.0.1
 cd ..
+
+docker rmi -f lapotkod/api-gateway:0.0.1
+cd .\api-gateway\
+docker build . -t lapotkod/api-gateway:0.0.1
+cd ..
+
 
 cd .\.docker\
 
